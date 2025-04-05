13ffbb861e11d149f00c521cec5338a812973f47
Andreas Rhode <m.a.rhode@gmail.com>
2025-04-05 01:41:27 -0400

    Create Main.java

---
+++ src/campus_quest/Main.java
@@ -0,0 +1,31 @@
+package campus_quest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		 
+		        CampusQuest game = new CampusQuest();
+
+              // Single console player start
+		        	Player player = new ConsolePlayer(new java.util.Scanner(System.in));
+		        	game.resetState();
+		        	player.play(game);  /
+             // Single console player end
+/* 		    
+              // For using RandomPlayer remove the single player code
+		        	// start a loop
+		        	Player player = new RandomPlayer();
+		        	game.resetState();
+		        	player.play(game);  
+		        	// check game outcome
+		        	System.out.println(game.getOutcome());
+		        // end a loop 
+		        // report number of winning and loosing games	
+*/
+	}
+}
